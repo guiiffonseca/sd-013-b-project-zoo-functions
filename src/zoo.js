2b05d4618b357032d71ffe5e93e3c070b58c29f6
mariananogueirab <mariananbp@gmail.com>
2021-06-30 17:12:19 -0300

    Refatorando Get Species By Id

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,6 @@
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  if (ids.length === 0) return [];
   return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
