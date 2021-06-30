a7379c22bb932eacf6dbf7befe66f8edb7799c63
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-30 17:43:41 -0400

    requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const { species, employees } = require('./data');
+const { species, employees, prices } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -31,16 +31,23 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function countAnimals(especies) {
-  let quantity = {};
+  const quantity = {};
   if (!especies) {
-    species.forEach((animal) => quantity[animal.name] = animal.residents.length);
+    species.forEach((animal) => (quantity[animal.name] = animal.residents.length));
     return quantity;
   } return species.find((specie) => specie.name === especies).residents.length;
-  
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const adPrice = prices.Adult;
+  const senPrice = prices.Senior;
+  const chiPrice = prices.Child;
+
+  if (!entrants || Object.entries(entrants).length === 0 ) return 0;
+  else {
+    const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+    return Adult * adPrice + Child * chiPrice + Senior * senPrice;
+  }
 }
 
 function getAnimalMap(options) {
