7f245f26b2d54688fc7d5ad516fa564efc5e7403
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-30 14:06:36 -0400

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,8 @@
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((animal) => animal.id === id));
 }
 
 function getAnimalsOlderThan(animal, age) {
