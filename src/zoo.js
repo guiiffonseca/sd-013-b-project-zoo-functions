252825763f59b5021a47edd388c46a2dbfbe0c7e
AC-Oliveira <allancardoso21@gmail.com>
2021-06-30 20:06:22 -0300

    Requisito 7 e anteriores.

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-// const { species } = require('./data');
+const { species, employees } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -6,15 +6,15 @@ function getSpeciesByIds(...ids) {
   if (!ids) return [];
   const array = [];
   ids.forEach((element) => {
-    array.push(data.species.find((string) => string.id === element));
+    array.push(species.find((string) => string.id === element));
   });
   return array;
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  let allAnimalsOlderThan = data.species.find((element) => element.name === animal).residents;
-  allAnimalsOlderThan = allAnimalsOlderThan.every((element) => element.age > age);
+  let allAnimalsOlderThan = data.species.find((element2) => element2.name === animal).residents;
+  allAnimalsOlderThan = allAnimalsOlderThan.every((element3) => element3.age > age);
   return allAnimalsOlderThan;
 }
 
@@ -22,30 +22,45 @@ function getEmployeeByName(employeeName) {
   // seu código aqui
   if (!employeeName) return {};
   const obj = data.employees.find(
-    (element) => element.firstName === employeeName || element.lastName === employeeName,
+    (element4) => element4.firstName === employeeName || element4.lastName === employeeName,
   );
   return obj;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
   // seu código aqui
-
+  return employees.some((element5) => {
+    const employeeIsManager = element5.managers.some((element6) => element6 === id);
+    return employeeIsManager === true;
+  });
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
-function countAnimals(species) {
+function countAnimals(animal) {
   // seu código aqui
+  let obj = {};
+  if (!animal) {
+    species.forEach((element7) => {
+      obj[element7.name] = element7.residents.length;
+    });
+  }
+  if (animal) {
+    obj = species.find((element8) => element8.name === animal).residents.length;
+  }
+  return obj;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+
 }
 
 function getAnimalMap(options) {
