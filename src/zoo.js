8e75bc227a97fd87860edb7daf2c6f5ece7099df
Marcos Estevao <dusdoom@gmail.com>
2021-06-30 16:51:29 -0300

    [Requisito 4]Adicionei o createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -18,7 +18,10 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return {
+    ...personalInfo,
+    ...associatedWith,
+  };
 }
 
 function isManager(id) {
