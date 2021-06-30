d516151db716928e457fd1bdeb0a4668c264438d
Jonmorais <jonathan_ms15@hotmail.com>
2021-06-30 17:01:53 -0300

    Requisito 1 :rocket:

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,16 @@
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const compare = ids;
+  const result = [];
+  species.filter((curr, index) => {
+    if (curr.id === compare[index]) {
+      result.push(curr);
+    }
+    return 0;
+  });
+  return result;
 }
 
 function getAnimalsOlderThan(animal, age) {
@@ -24,7 +33,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals() {
   // seu código aqui
 }
 
