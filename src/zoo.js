70f698eb5f15e35e7391eff89ce2cd76a893696a
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-30 23:20:05 +0100

    requisito 2 refat

--- src/zoo.js
+++ src/zoo.js
@@ -8,8 +8,9 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  return data.species.find((specie) => specie.name === animal).residents.every((resident) => resident.age >= age);
-  // data é objeto, .species retorna array, 
+  const residentAges = data.species.find((specie) => specie.name === animal).residents;
+  return residentAges.every((resident) => resident.age >= age);
+  // data é objeto, .species retorna array.
   // Fiz este requisito acompanhando o raciocinio do Nikolai no plantão do Panta no dia 30/06.
 }
 
