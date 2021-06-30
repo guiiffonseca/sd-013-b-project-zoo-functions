9ecbb91b31017d17a4eff2222b48a2c9afebbfd4
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-06-30 18:32:47 -0300

    Requisito 3 funcionando

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const { species } = require('./data');
+const { species, employees } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -13,7 +13,9 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find((employee) => employee.firstName === employeeName
+  || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
