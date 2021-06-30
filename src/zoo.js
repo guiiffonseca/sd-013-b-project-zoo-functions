838e1215844a1ba963ef06c1966d63a35bf5d724
diogodlemos <diogodrummond@hotmail.com>
2021-06-30 17:26:01 -0300

    Requesito 1 ao 4 feitos- Iniciando Projeto Zoo Functions

--- src/zoo.js
+++ src/zoo.js
@@ -1,30 +1,55 @@
+const { species, employees } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids === undefined) return [];
+  return species.filter(({ id }) => ids.includes(id));
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const specieSelected = species.find(({ name }) => name === animal);
+  const animmalsSelected = specieSelected.residents;
+  const count = animmalsSelected.filter((animmalSelected) => animmalSelected.age >= age);
+  if (count.length === animmalsSelected.length) return true;
+  return false;
 }
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) return {};
+  return employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return {
+    id: `${id}`,
+    firstName: `${firstName}`,
+    lastName: `${lastName}`,
+    managers: `${managers}`.split(/\s*,\s*/),
+    responsibleFor: `${responsibleFor}`.split(/\s*,\s*/),
+  };
 }
 
 function isManager(id) {
   // seu código aqui
+  const employeeSelected = employees.find(({ id }) => id === id);
+  console.log(employeeSelected);
+  if(employeeSelected.managers === []) return false;
+  return true;
 }
+
+isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83');
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals(speciesParam) {
   // seu código aqui
 }
 
