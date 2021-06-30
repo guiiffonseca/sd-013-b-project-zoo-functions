2fc4d3c0d0fad695e0c8744d52cf617eff9d1edd
Yg.Or <inygu@icloud.com>
2021-06-30 15:17:33 -0300

    requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -36,7 +36,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith;
+  return {...personalInfo, ...associatedWith};
 }
 
 function isManager(id) {
