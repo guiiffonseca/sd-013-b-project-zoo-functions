33558a0d0500f4029a023c8d94a0dd287c9b9d98
Thiago <thiagodesouza.dev@gmail.com>
2021-06-30 20:12:45 -0300

    addEmployee finalizado

--- src/zoo.js
+++ src/zoo.js
@@ -21,8 +21,16 @@ function isManager(id) {
   // seu código aqui
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  const employee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  data.employees.push(employee);
 }
 
 function countAnimals(species) {
