cf4454fa75654563d1f47c4d65de09fcd2c9d447
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-30 22:16:23 +0100

    requisito 1 refat

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const data = require("./data");
+const data = require('./data');
 
 function getSpeciesByIds(...ids) {
   return ids.map((id) => data.species.find((specie) => specie.id === id));
@@ -6,7 +6,6 @@ function getSpeciesByIds(...ids) {
   // map criou outro array
   // para tirar um único e determinado elemento do array usei o find
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
