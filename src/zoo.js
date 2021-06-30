4f1762d1a72bd523a7f00e8da09c01a5ca2339ba
Pedro Brito <pedroab0119@gmail.com>
2021-06-30 18:29:05 -0300

    rqe1 done

--- src/zoo.js
+++ src/zoo.js
@@ -1,8 +1,9 @@
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  if (!ids) return []
-  return ids;
+function getSpeciesByIds(...ids) {
+  return ids.map((valueIds) => {
+    return data.species.find((animalSpecie) => animalSpecie.id === valueIds);
+  });
 }
 
 function getAnimalsOlderThan(animal, age) {
@@ -68,8 +69,9 @@ module.exports = {
   increasePrices,
   createEmployee,
 };
-console.log('caso 1');
-console.log(getSpeciesByIds());
-console.log('caso 2');
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-console.log('caso 3');
+// console.log('caso 1 - []');
+// console.log(getSpeciesByIds());
+// console.log('caso 2 - 0938aa23-f153-4937-9f88-4858b24d6bce');
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log('caso 3 - 0938aa23-f153-4937-9f88-4858b24d6bce, e8481c1d-42ea-4610-8e11-1752cfc05a46');
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
