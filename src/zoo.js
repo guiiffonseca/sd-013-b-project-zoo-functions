19dc51c766329a44845c4fa0c4fcfb73915b4b9d
marcus <marcus.rodrigues10@hotmail.com>
2021-06-30 15:26:34 -0300

    exercise 4

--- src/zoo.js
+++ src/zoo.js
@@ -31,6 +31,7 @@ function getEmployeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
