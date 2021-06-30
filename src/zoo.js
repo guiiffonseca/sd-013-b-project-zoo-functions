3d4a11702ef3d57b048ef3c7a17b52b47fa7b277
Yg.Or <inygu@icloud.com>
2021-06-30 18:29:55 -0300

    requisito 9

--- src/zoo.js
+++ src/zoo.js
@@ -107,8 +107,29 @@ function getAnimalMap(options) {
   // seu código aqui
 }
 
+function allDays() {
+  const chaves = Object.keys(data.hours);
+    const horario = {};
+    chaves.forEach((dia) => {
+      if (data.hours[dia].open === 0 && data.hours[dia].close === 0) {
+        horario[`${dia}`] = 'CLOSED';
+      } else {
+        horario[`${dia}`] = `Open from ${data.hours[dia].open}am until ${data.hours[dia].close - 12}pm`;
+      }
+    });
+    return horario;
+
+}
+
 function getSchedule(dayName) {
-  // seu código aqui
+  const days = allDays();
+  if (!dayName) {
+    return days;
+  }
+  return {
+    [dayName]: `${days[dayName]}`;
+  }
+
 }
 
 function getOldestFromFirstSpecies(id) {
