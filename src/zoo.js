94cb9c3790b7246aee861588272b712b568c404e
Henrique Cursino <henriquecursino46@gmail.com>
2021-06-30 18:02:53 -0300

    Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -25,6 +25,10 @@ function getEmployeeByName(employeeName) {
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return {
+    ...personalInfo,
+    ...associatedWith,
+  };
 }
 
 function isManager(id) {
