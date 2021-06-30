c51790459fd57c897080381f2b75ccc2b833dae6
mariananogueirab <mariananbp@gmail.com>
2021-06-30 17:11:10 -0300

    Get Species By Id

--- src/zoo.js
+++ src/zoo.js
@@ -1,11 +1,8 @@
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  
   if (ids.length === 0) return [];
-  
   return ids.map((id) => data.species.find((specie) => specie.id === id));
-  
 }
 
 function getAnimalsOlderThan(animal, age) {
