d0da614908edb72b0fc365c331dc78b5a1057c90
Leonardo Martins <leonardomartins07@gmail.com>
2021-07-01 02:39:37 -0300

    part 2

--- src/zoo.js
+++ src/zoo.js
@@ -6,7 +6,8 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalCheck = species.find((specie) => specie.name === animal);
+  return animalCheck.residents.every((resident) => resident.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
