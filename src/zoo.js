9c34b472b88fab7701d6995179c297888b5d6758
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-01 06:19:02 -0300

    resolvendo o req 4

--- src/zoo.js
+++ src/zoo.js
@@ -29,7 +29,14 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const employee = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor,
+  };
+  return employee;
 }
 
 function isManager(id) {
