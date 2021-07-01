b75ccdb2a8e9a32303cd95481abd44d9356362a4
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-07-01 13:17:56 -0300

    Adiciona requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -30,7 +30,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  
+  return ({ ...personalInfo, ...associatedWith });
 }
 
 function isManager(id) {
