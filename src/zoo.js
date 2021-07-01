be466fa060f49d423b30263612137c31110bbd58
LúcioBJ <lbjunq@hotmail.com>
2021-07-01 17:58:14 -0300

    Adicionado o decimo e decimo primeiro requisito

--- src/zoo.js
+++ src/zoo.js
@@ -2,6 +2,7 @@ const data = require('./data');
 
 const { species, employees, hours, prices } = data;
 
+// 1
 function getSpeciesByIds(...ids) {
   // seu código aqui
   // species.filter((specie) => specie === ids.some((id) => specie.id === id))
@@ -9,12 +10,14 @@ function getSpeciesByIds(...ids) {
 
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
+// 2
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   const searchedSpecies = species.find((animals) => (animals.name === animal));
   return searchedSpecies.residents.every((resident) => resident.age > age);
 }
 
+// 3
 function getEmployeeByName(employeeName = '') {
   // seu código aqui
   if (employeeName === '') {
@@ -23,6 +26,7 @@ function getEmployeeByName(employeeName = '') {
   return employees.find((p) => (p.firstName === employeeName || p.lastName === employeeName));
 }
 
+// 4
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
   return {
@@ -31,12 +35,14 @@ function createEmployee(personalInfo, associatedWith) {
   };
 }
 
+// 5
 function isManager(id) {
   // seu código aqui
   const managers = employees.reduce((result, managerId) => [...result, ...managerId.managers], []);
   return managers.includes(id);
 }
 
+// 6
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
   return employees.push({
@@ -48,6 +54,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   });
 }
 
+// 7
 function countAnimals(speciesToCount) {
   // seu código aqui
   const animalCount = {};
@@ -58,6 +65,7 @@ function countAnimals(speciesToCount) {
   return animalCount;
 }
 
+// 8
 function calculateEntry(entrants = {}) {
   // seu código aqui
   let finalPrice = 0;
@@ -73,43 +81,56 @@ function calculateEntry(entrants = {}) {
   return finalPrice;
 }
 
+// 9
 function getAnimalMap(options) {
   // seu código aqui
 }
 
-function getSchedule(dayName) {
+// 10
+function getSchedule(dayName = '') {
   // seu código aqui
-  // const schedule = {};
-  // hours.forEach((day) => { schedule[day] = `Open from ${day.open} until ${day.close}`});
-  // console.log(schedule);
-  // schedule.forEach((day === 0) ? 'CLOSED' : day);
-  // if (schedule[dayName]) {
-  //   return schedule[dayName];
-  // }
-  // return schedule;
+  const schedule = {};
+  const weekday = Object.keys(hours);
+  const time = Object.values(hours);
+  for (let index = 0; index < time.length; index += 1) {
+    schedule[weekday[index]] = `Opens at ${time[index].open}am until ${time[index].open}pm`;
+    if (time[index].open === 0 && time[index].close === 0) {
+      schedule[weekday[index]] = 'CLOSED';
+    }
+  }
+  if (dayName === '') {
+    return schedule;
+  }
+  return { dayName: schedule[dayName] };
 }
 
+// 11
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-  // const speciesId = employees
-  // .find((employee) => (employee.id === id))
-  // .responsibleFor[0];
-  // const specieObj = species.find((specie) => specie.id === speciesId);
-  // currentAge = 0;
-  // specieObj.residents.forEach((ind) => {
-  //   if (ind.age > currentAge) {
-  //     currentAge = ind.age;
-  //   }
-  // });
-  // Oldest = specieObj.find((animal) => animal.age === currentAge);
-  // return [...Oldest]
+  const speciesId = employees
+    .find((employee) => (employee.id === id))
+    .responsibleFor[0];
+  const residents = species
+    .find((specie) => specie.id === speciesId)
+    .residents;
+  let oldestAge = 0;
+  residents.forEach((ind) => {
+    if (ind.age > oldestAge) {
+      oldestAge = ind.age;
+    }
+  });
+  const Oldest = residents.find((ind) => ind.age === oldestAge);
+  const final = [Oldest.name, Oldest.sex, Oldest.age];
+  return final;
 }
 
+// 12
 function increasePrices(percentage) {
   // seu código aqui
   // prices.forEach((category) => prices[category] *= (1 + (percentage/100)));
 }
 
+// 13
 function getEmployeeCoverage(idOrName) {
   // seu código aqui
   // const employee =
