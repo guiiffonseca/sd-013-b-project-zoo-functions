f16b31820b75fb5ce26f26bf2a5d48b7772cdb1f
Haron Prestes <haronprestes1303@gmail.com>
2021-07-02 18:38:54 -0300

    Requisito 5.

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,9 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some(() => id === '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  || id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8'
+  || id === '0e7b460e-acf4-4e17-bcb3-ee472265db83');
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
