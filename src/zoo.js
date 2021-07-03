827b7c3f9e73b333fd1566a4780c7babb231aa99
José Breno <brenojbs@hotmail.com>
2021-07-03 10:27:55 -0300

    Requisito 4 pronto(Lint com erro 4:34)

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
