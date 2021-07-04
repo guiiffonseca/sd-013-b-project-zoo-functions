3f61385594fac0917b087e62f63e3af6dc93f32f
SaraRabelo <rabelorsara@gmail.com>
2021-07-04 00:31:43 -0300

    Corrigindo  a 1a Questão...

--- src/zoo.js
+++ src/zoo.js
@@ -1,15 +1,14 @@
 const data = require('./data');
 
-function getSpeciesByIds(...ids) { 
-  let arrayOfSpecies = [];
+function getSpeciesByIds(...ids){ 
+  const arrayOfSpecies = [];
   ids.forEach((element) => {
-     let a = data.species.find((specie)=>specie.id === element);
-     arrayOfSpecies.push(a);
-     return arrayOfSpecies;
+    arrayOfSpecies.push(data.species.find((specie)=>specie.id === element));
+    return arrayOfSpecies;
   });
   console.log(arrayOfSpecies);
 };
-console.log(getSpeciesByIds(...['0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46']));
+console.log(getSpeciesByIds(...['0938aa23-f153-4937-9f88-4858b24d6bce','e8481c1d-42ea-4610-8e11-1752cfc05a46']));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
