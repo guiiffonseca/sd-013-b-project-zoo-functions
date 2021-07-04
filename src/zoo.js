73056b827abe046a3ddc0dd7cb4dbfcd2a497ddf
Luishsk93 <hyungsunkangkr@gmail.com>
2021-07-04 19:09:13 -0300

    Requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -1,77 +1,34 @@
+// Usei como referência o projeto do Rafael Nery Machado para compactar funcoes feitas.
+
 const { species, employees, hours, prices } = require('./data');
 
 function getSpeciesByIds(...ids) {
-  // const [firstId, secondId] = ids;
-  // const specieOfFirstId = data.species.filter((specie) => specie.id === firstId);
-  // const specieOfSecondId = data.species.filter((specie) => specie.id === secondId);
-  // const speciesByIds = [...specieOfFirstId, ...specieOfSecondId];
-  // return speciesByIds;
-  // Usei como referência o projeto do Rafael Nery Machado na modificação da função para uma versão mais compacta
   return species.filter((specie) => ids.includes(specie.id));
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
   return species.find((Animal) => Animal.name === animal)
     .residents.every((foundAnimal) => foundAnimal.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
-  // if (typeof employeeName !== 'string') {
-  //   return {};
-  // }
-  // Usei como referência o projeto do Rafael Nery Machado na modificação da função para uma versão mais compacta
   return employees.find((employee) =>
     employee.firstName === employeeName || employee.lastName === employeeName || {});
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui.
-  // const { id, firstName, lastName } = personalInfo;
-  // const { managers, responsibleFor } = associatedWith;
-  // const newEmployee = (employeeId,
-  //   employeeIdFirstName, employeeIdlastName,
-  //   employeeIdmanagers, employeeIdresponsibleFor) => ({
-  //   id,
-  //   firstName,
-  //   lastName,
-  //   if (associatedWith === undefined) {
-  //     managers,
-  //     responsibleFor,
-  //   }
-  // });
-  // return newEmployee(id, firstName, lastName, managers, responsibleFor);
-  // Usei como referência o projeto do Rafael Nery Machado na modificação da função para uma versão mais compacta
   return { ...personalInfo, ...associatedWith };
 }
 
-// function flatten(arrays) {
-//   // Função auxiliar que transforma uma matriz em um vetor
-//   return arrays.reduce((accumulator, currentValue) => {
-//     for (let i = 0; i < currentValue.length; i += 1) {
-//       accumulator.push(currentValue[i]);
-//     }
-//     return accumulator;
-//   });
-// }
-
 function isManager(id) {
-  // seu código aqui
-  // const managers = data.employees.map((person) => person.managers);
-  // return flatten(managers).some((manager) => manager === id);
-  // Usei como referência o projeto do Rafael Nery Machado na modificação da função para uma versão mais compacta
-  // Outra referencia - https://brianflove.com/2014-09-02/whats-the-double-exclamation-mark-for-in-javascript/#:~:text=If%20you%20have%20ever%20noticed,(true%20or%20false)%20value.
   return !!employees.find(({ managers }) => managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  // seu código aqui
   employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(Specie) {
-  // seu código aqui
   const animalsQtd = {};
   species.forEach((animal) => { animalsQtd[animal.name] = animal.residents.length; });
   if (!Specie) {
@@ -81,30 +38,60 @@ function countAnimals(Specie) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
   if (entrants === undefined || entrants === {}) return 0;
   const { Adult = 0, Senior = 0, Child = 0 } = entrants;
   return (Adult * prices.Adult + Senior * prices.Senior + Child * prices.Child);
 }
 
 function getAnimalMap(options) {
-  // seu código aqui
+  //
+}
+
+function formatDays() {
+  const formattedDays = { };
+  const daysArr = Object.keys(hours);
+  daysArr.forEach((day) => {
+    const openHour = hours[day].open;
+    const closeHour = hours[day].close;
+    if (openHour === 0 && closeHour === 0) {
+      formattedDays[day] = 'CLOSED';
+    } else {
+      formattedDays[day] = `Open from ${openHour}am until ${(closeHour > 12) ? closeHour - 12
+        : closeHour}pm`;
+    }
+  });
+  return formattedDays;
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const newSchedule = formatDays();
+  if (dayName === undefined) return newSchedule;
+  return { [dayName]: newSchedule[dayName] };
 }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(Id) {
+  const employee = employees.find(({ id }) => id.includes(Id));
+  const targetAnimalId = employee.responsibleFor[0];
+  const targetAnimal = species.find(({ id }) => id.includes(targetAnimalId));
+  const oldestTargetAnimal = targetAnimal.residents
+    .reduce((acc, value) => {
+      if (value.age > acc.age) {
+        return value;
+      } return acc;
+    });
+  return Object.values(oldestTargetAnimal);
 }
 
+// https://stackoverflow.com/questions/6134039/format-number-to-always-show-2-decimal-places
 function increasePrices(percentage) {
-  // seu código aqui
+  prices.Adult = parseFloat((Math.ceil(prices.Adult * (100 + percentage)) / 100).toFixed(2));
+  prices.Senior = parseFloat((Math.ceil(prices.Senior * (100 + percentage)) / 100).toFixed(2));
+  prices.Child = parseFloat((Math.ceil(prices.Child * (100 + percentage)) / 100).toFixed(2));
+  return prices;
 }
 
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
+  // const getEmployee = getEmployeeByName(idOrName) === {} ?   
 }
 
 module.exports = {
