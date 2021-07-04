0216b646b612c30163f467deede2bff9e5c7968d
SaraRabelo <rabelorsara@gmail.com>
2021-07-04 20:57:59 -0300

    6a Questão Resolvida

--- src/zoo.js
+++ src/zoo.js
@@ -27,9 +27,13 @@ function isManager(id) {
   // seu código aqui
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor =[]) {
+  return data.employees.push({id, firstName, lastName, managers, responsibleFor});
+  /*data.employees.firstName.push(firstName);
+  data.employees.lastName.push(lastName);
+  data.employees.managers.push(managers);
+  data.employees.responsibleFor.push(responsibleFor);*/
+  }
 
 function countAnimals(species) {
   // seu código aqui
