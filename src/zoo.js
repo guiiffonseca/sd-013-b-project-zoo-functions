7feb320275f5b41f2e51b25687f443fd7561c635
mariananogueirab <mariananbp@gmail.com>
2021-07-05 17:17:28 -0300

    Get Employee Coverage

--- src/zoo.js
+++ src/zoo.js
@@ -165,21 +165,22 @@ function increasePrices(percentage) {
   pricesEntries.forEach((type) => {
     const percentageRight = (percentage + 100) / 100;
     const newPrice = parseFloat(data.prices[type]) * percentageRight;
-    console.log(newPrice);
     data.prices[type] = Math.round(newPrice * 100) / 100;
   });
 }
 
-function getEmployeeCoverage(idOrName) {
+const getEmployeeWithoutidOrName = (idOrName) => {
   const result = {};
   const ids = {
-    '0938aa23-f153-4937-9f88-4858b24d6bce': 'lionId',
-    '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae': 'ottersId',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5': 'elephantsId',
-    '78460a91-f4da-4dea-a469-86fd2b8ccc84': 'snakesId',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274': 'frogsId',
-    'baa6e93a-f295-44e7-8f70-2bcdc6f6948d': 'bearsId',
-    'e8481c1d-42ea-4610-8e11-1752cfc05a46': 'tigersId',
+    '0938aa23-f153-4937-9f88-4858b24d6bce': 'lions',
+    '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae': 'otters',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5': 'elephants',
+    '78460a91-f4da-4dea-a469-86fd2b8ccc84': 'snakes',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274': 'frogs',
+    'baa6e93a-f295-44e7-8f70-2bcdc6f6948d': 'bears',
+    'e8481c1d-42ea-4610-8e11-1752cfc05a46': 'tigers',
+    'ef3778eb-2844-4c7c-b66c-f432073e1c6b': 'penguins',
+    '01422318-ca2d-46b8-b66c-3e9e188244ed': 'giraffes',
   };
   if (!idOrName) {
     data.employees.forEach(({ firstName, lastName, responsibleFor }) => {
@@ -187,8 +188,29 @@ function getEmployeeCoverage(idOrName) {
     });
   }
   return result;
+};
+
+function getEmployeeCoverage(idOrName) {
+  const result = {};
+  const ids = {
+    '0938aa23-f153-4937-9f88-4858b24d6bce': 'lions',
+    '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae': 'otters',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5': 'elephants',
+    '78460a91-f4da-4dea-a469-86fd2b8ccc84': 'snakes',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274': 'frogs',
+    'baa6e93a-f295-44e7-8f70-2bcdc6f6948d': 'bears',
+    'e8481c1d-42ea-4610-8e11-1752cfc05a46': 'tigers',
+    'ef3778eb-2844-4c7c-b66c-f432073e1c6b': 'penguins',
+    '01422318-ca2d-46b8-b66c-3e9e188244ed': 'giraffes',
+  };
+  if (!idOrName) return getEmployeeWithoutidOrName(idOrName);
+  const employee = data.employees.find(({ id, firstName, lastName }) =>
+    idOrName === id || idOrName === firstName || idOrName === lastName);
+  result[`${employee.firstName} ${employee.lastName}`] = employee.responsibleFor.map((idAnimal) =>
+    ids[idAnimal]);
+  return result;
 }
-console.log(getEmployeeCoverage());
+
 module.exports = {
   calculateEntry,
   getSchedule,
