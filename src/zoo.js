5a2e020d59187e6c49ee107e8efca973c4a1985e
Demontier <demontier@outlook.com>
2021-07-05 07:27:38 -0300

    Requisito 9

--- src/zoo.js
+++ src/zoo.js
@@ -1,6 +1,7 @@
 const data = require('./data');
 
 const { species, employees, prices } = data;
+const byLocation = {};
 
 function getSpeciesByIds(...params) {
   // seu código aqui
@@ -55,8 +56,86 @@ function calculateEntry(entrants = 0) {
   return Adult * adult + Senior * senior + Child * child;
 }
 
+// função que retorna animais categorizados por localização
+const animaBaylLocation = (animalLocation) => {
+  const arrayByName = [];
+  species.map(({ name, location }) => {
+    if (animalLocation === location) {
+      arrayByName.push(name);
+    }
+    return undefined;
+  });
+  byLocation[animalLocation] = arrayByName;
+  return byLocation;
+};
+
+// Com a opção `includeNames: true` e `sorted: true`(opcional) especificada, retorna nomes de animais'
+const animalBayName = (animalLocation, sorted) => {
+  const arrayByName = [];
+  species.map(({ name, location, residents }) => {
+    const nameAnimal = {};
+    if (animalLocation === location && sorted === false) {
+      nameAnimal[name] = residents.map((resident) => resident.name);
+      arrayByName.push(nameAnimal);
+    }
+    if (animalLocation === location && sorted === true) {
+      nameAnimal[name] = residents.map((resident) => resident.name).sort();
+      arrayByName.push(nameAnimal);
+    }
+    return undefined;
+  });
+  byLocation[animalLocation] = arrayByName;
+  return byLocation;
+};
+
+const animalBaysex = (animalLocation, sorted, animalSex) => {
+  const arrayBySex = [];
+  species.map(({ name, location, residents }) => {
+    const sexAnimal = {};
+    if (animalLocation === location && sorted === true) {
+      sexAnimal[name] = residents.filter(({ sex }) => sex === animalSex)
+        .map((residentName) => residentName.name).sort();
+      arrayBySex.push(sexAnimal);
+    }
+    if (animalLocation === location && sorted === false) {
+      sexAnimal[name] = residents.filter(({ sex }) => sex === animalSex)
+        .map((residentName) => residentName.name);
+      arrayBySex.push(sexAnimal);
+    }
+    return undefined;
+  });
+  byLocation[animalLocation] = arrayBySex;
+  return byLocation;
+};
+
+// cria novo catalago
+const newCatalog = (callback, sorted, sex) => {
+  species.map(({ location }) => callback(location, sorted, sex));
+  return byLocation;
+};
+
+//-------------------------------------------------------------------------
+const sortedOn = (options) => {
+  if (options.sex === 'female' || options.sex === 'male') {
+    return newCatalog(animalBaysex, true, options.sex);
+  }
+  return newCatalog(animalBayName, true, options.sex);
+};
+
+const sortedOff = (options) => {
+  if (options.sex === 'female' || options.sex === 'male') {
+    return newCatalog(animalBaysex, false, options.sex);
+  }
+  return newCatalog(animalBayName, false);
+};
+
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options || !options.includeNames) {
+    return newCatalog(animaBaylLocation);
+  }
+  if (options.sorted) return sortedOn(options);
+  if (!options.sorted) return sortedOff(options);
 }
 
 function getSchedule(dayName) {
