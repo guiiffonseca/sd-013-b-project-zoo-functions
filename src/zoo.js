fb2854da10ea4e7d1c63f7a1170b91b1644a804f
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-07-05 18:20:33 -0300

    RQ 3 - IMPLEMENTE A FUNÇÃO employeeName

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const { species } = require('./data');
+const { species, employees } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -12,7 +12,9 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find((worker) => worker.firstName === employeeName) || 
+  employees.find((worker) => worker.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
