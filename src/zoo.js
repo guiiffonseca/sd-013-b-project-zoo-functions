18aed7d65c54cd024ddfd97fc32cf6fc0957c906
SaraRabelo <rabelorsara@gmail.com>
2021-07-04 22:02:49 -0300

    5a Questão...

--- src/zoo.js
+++ src/zoo.js
@@ -25,7 +25,10 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  if (id === "0e7b460e-acf4-4e17-bcb3-ee472265db83") {
+    return true;
+  }
+  return false;
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
