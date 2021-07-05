f2150758dae653b9589962092af0dd088bb308ce
Natalia Camarinha <natcamarinha@gmail.com>
2021-07-05 16:31:13 -0300

    requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -80,7 +80,7 @@ function countAnimals(species) {
 // o forEach vai executar a função em cada elemento name e residents e para cada espécie eu pego o nome e o número de reidentes.
 
 function calculateEntry(entrants) {
-  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  if (!entrants || entrants === 0) return 0;
 
   const entries = Object.entries(entrants);
   return entries.reduce((acc, personType) =>
@@ -88,15 +88,44 @@ function calculateEntry(entrants) {
 }
 
 // se não tiver parâmetro ou o objeto for vazio retorna 0.
-// criei uma constante que pega todas as entradas do obejto num array, por exemplo ['adult', '2'], depois faço um reduce pegando esse array. o acumulador soma (com o index[1] desse array que é o numero de pessoas multiplicado pelo preço de cada pessoa no index[0]). o último 0 faz parte da sintaxe do reduce, onde a soma começa com 0.
+// criei uma constante que pega todas as entradas do objeto num array, por exemplo ['adult', '2'], depois faço um reduce pegando esse array. o acumulador soma (com o index[1] desse array que é o numero de pessoas multiplicado pelo preço de cada pessoa no index[0]). o último 0 faz parte da sintaxe do reduce, onde a soma começa com 0.
 
 function getAnimalMap(options) {
   // seu código aqui
 }
 
-function getSchedule(dayName) {
-  // seu código aqui
+function scheduleAllDays(dayName) {
+  const schedule = {};
+  const days = Object.entries(data.hours);
+  if (!dayName) {
+    days.forEach((day) => {
+      if (day[0] === 'Monday') schedule[day[0]] = 'CLOSED';
+      else schedule[day[0]] = `Open from ${day[1].open}am until ${day[1].close - 12}pm`;
+    });
+  }
+  return schedule;
 }
+
+function scheduleOneDay(dayName) {
+  const schedule = {};
+  const days = Object.entries(data.hours);
+  const specificDay = days.find((day) => day[0] === dayName);
+  if (dayName === 'Monday') schedule[dayName] = 'CLOSED';
+  else {
+    schedule[dayName] = `Open from ${specificDay[1].open}am until ${specificDay[1].close - 12}pm`;
+  }
+  return schedule;
+}
+
+function getSchedule(dayName) {
+  if (!dayName) return scheduleAllDays();
+  return scheduleOneDay(dayName);
+}
+
+// fiz 2 funções adicionais pra pegar cada um dos testes.
+// a primeira função scheduleAllDays é para caso não seja passado nenhum parâmetro. fiz uma constante schedule = {} para trazer o resultado depois da função e outra função com o array de array das informações do objeto prices. se não tiver parâmetro (!dayName) faço um forEach para buscar as informações, se o day no index[0] for 'Monday' está 'CLOSED'. se o day no index[0] for outro dia será o o horário de funcionamento. e dentro da função eu retorno o schedule com o resultado.
+// a segunda função scheduleOneDay é para caso seja passado um dia como parâmetro. fiz uma constante schedule = {} para trazer o resultado depois da função e outra função com o array de array das informações do objeto prices. fiz um find para encontrar dentro desse array o parâmetro passado day[0] === dayName. se o day no index[0] for 'Monday' está 'CLOSED'. se o day no index[0] for outro dia será o o horário de funcionamento. e dentro da função eu retorno o schedule com o resultado.
+// a função getSchedule retorna: a função scheduleAllDays se não tiver parâmetro ou a função scheduleOneDay passando o parâmetro.
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
