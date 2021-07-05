f860a4aa05c37c70f057c2f7c5061ad7a559402f
Mariana Garcia <maridgarcia045@gmail.com>
2021-07-05 19:09:20 -0300

    Requisito getSchedule

--- src/zoo.js
+++ src/zoo.js
@@ -1,6 +1,6 @@
 const data = require('./data');
 
-const { species, employees } = data;
+const { species, employees, hours } = data;
 
 function getSpeciesByIds(...ids) {
   return species.filter((specie) => ids.includes(specie.id));
@@ -45,7 +45,7 @@ function countAnimals(countSpecies) {
 }
 
 function calculateEntry(entrants) {
-  if (!entrants || entrants === {}) return 0;
+  if (!entrants) return 0;
   const { Adult = 0, Child = 0, Senior = 0 } = entrants;
 
   const total = Adult * 49.99 + Senior * 24.99 + Child * 20.99;
@@ -56,8 +56,22 @@ function getAnimalMap(options) {
   // seu código aqui
 }
 
+// Passado o dia como parâmetro, retorna somente o dia.
+
 function getSchedule(dayName) {
-  // seu código aqui
+  if (!dayName) {
+    return {
+      Tuesday: `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close - 12}pm`,
+      Wednesday:`Open from ${hours.Wednesday.open}am until ${hours.Wednesday.close - 12}pm`,
+      Thursday:`Open from ${hours.Thursday.open}am until ${hours.Thursday.close - 12}pm`,
+      Friday:`Open from ${hours.Friday.open}am until ${hours.Friday.close - 12 }pm`,
+      Saturday:`Open from ${hours.Saturday.open}am until ${hours.Saturday.close - 12}pm`,
+      Sunday:`Open from ${hours.Sunday.open}am until ${hours.Sunday.close - 12}pm`,
+      Monday:`CLOSED`,
+    }
+  }
+  if (dayName === 'Monday') return { 'Monday': 'CLOSED' }
+  return { [dayName]: `Open from ${hours[dayName].open}am until ${hours[dayName].close - 12}pm` };
 }
 
 function getOldestFromFirstSpecies(id) {
