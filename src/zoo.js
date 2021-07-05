ebb9862f9c11dde74bfe69d7d5605301625b3af8
bvottri <bruna.votre@gmail.com>
2021-07-05 00:51:27 -0300

    Requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -1,11 +1,19 @@
 const data = require('./data');
-
+// Com a ajuda da Mariana Nogueira, consegui fazer o primeiro requisito:
+// Usei o rest para que independente de quantos parâmetros a função funcione;
+// Coloquei a função para retornar o parâmetro com map;
+// O map percorre o parametro e retorna ele como um array;
+// Dentro do map coloquei um find para encontrar dentro de species o id que é igual a ao parametro;
 function getSpeciesByIds(...ids) {
   return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
+// 1- Encontrar o animal(animal);
+// 2- Encontrar as idades;
+// 3- Ver se as idades são maiores que a idade minima(age);
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return (data.species.find((specie) => specie.name === animal))
+    .residents.every((minAge) => minAge.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
