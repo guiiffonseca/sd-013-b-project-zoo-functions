95e277469fd8c73270d1f783b632455c7218ef0d
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-07-05 22:22:47 -0300

    Requisitos 1 a 5

--- src/zoo.js
+++ src/zoo.js
@@ -13,9 +13,7 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  if (employeeName === undefined) {
-    return {};
-  }
+  if (employeeName === undefined) return {};
   return employees.find(
     (employee) =>
       employeeName === employee.firstName || employeeName === employee.lastName,
@@ -23,11 +21,14 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  // Consultado em: https://stackoverflow.com/questions/39121695/merge-two-objects-with-es6
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
-  // seu código aqui
+  const managersIds = ['9e7d4524-363c-416a-8759-8aa7e50c0992',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8', '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
+  return !!managersIds.find((managerId) => managerId === id);
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
