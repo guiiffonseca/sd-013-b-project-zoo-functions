aa3f5e1562d69a2ee9a08eb0452c91c873c5d8ad
AquilesDantas <aquilesdant@gmail.com>
2021-07-06 00:01:45 -0300

    Requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -54,7 +54,11 @@ function countAnimals(species) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+  return (data.prices.Adult * Adult) + (data.prices.Child * Child) + (data.prices.Senior * Senior);
 }
 
 function getAnimalMap(options) {
