90af55a2bacd678effce2091a19fcb648c4ac32c
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-07-06 10:21:42 -0300

    retornando

--- src/zoo.js
+++ src/zoo.js
@@ -1,18 +1,22 @@
 const data = require('./data');
+// const { species } = require('./data');
+// const { species, employees, hours, prices }  data;
+
 
 function getSpeciesByIds(...ids) {
   if (!ids) return []; // retorna vazio se não entrar parametro
-  return data.species.filter((specie) => ids.some((id) => specie.id === id));
+  return data.species.filter((specie) => ids.some((id) => id === specie.id ));
 }
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animalName, age) {
+species.find((animal) => animal.name === animalName)
+  .residents.every((specie) => specie.age > age)
 }
 
 function getEmployeeByName(employeeName) {
   if (!employeeName) return {};
   return data.employees.find((employee) => employee.firstName === employeeName 
-|| employee.lastName === employeeName);
+  || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
