e597bcaa49e51759772fa647c086d6b48974ef61
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-07-06 10:35:09 -0300

    retornando linter

--- src/zoo.js
+++ src/zoo.js
@@ -2,22 +2,21 @@ const data = require('./data');
 // const { species } = require('./data');
 // const { species, employees, hours, prices }  data;
 
-
 function getSpeciesByIds(...ids) {
   if (!ids) return []; // retorna vazio se não entrar parametro
-  return data.species.filter((specie) => ids.some((id) => id === specie.id ));
+  return data.species.filter((specie) => ids.some((id) => id === specie.id));
 }
 
 function getAnimalsOlderThan(animalName, age) {
-species.find((animal) => animal.name === animalName)
+  data.species.find((animal) => animal.name === animalName)
   .residents.every((specie) => specie.age > age)
-}
+};
 
 function getEmployeeByName(employeeName) {
   if (!employeeName) return {};
-  return data.employees.find((employee) => employee.firstName === employeeName 
-  || employee.lastName === employeeName);
-}
+  return data.employees.find((employee) => 
+  employee.firstName === employeeName || employee.lastName === employeeName);
+};
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
