4b21a661997920bcab35bedcfb43e568b0a04c77
YasminAraujo3 <yasmin.nunesaraujo@gmail.com>
2021-07-06 20:05:24 -0300

    Requisito 1 feito

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,8 @@
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((specie) => specie.id === id))
 }
 
 function getAnimalsOlderThan(animal, age) {
