37f4f0d24325a951b9d5c78028ecee641eebd51b
Letícia Castro <aicitelks@gmail.com>
2021-07-06 15:39:07 -0300

    Ajustes do linter 2 (req. 7)

--- src/zoo.js
+++ src/zoo.js
@@ -95,15 +95,16 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 function countAnimals(species) {
   // Ajuda do Cajueiro | todasEspecies é o meu obj vazio {}
   const contagemAnimal = especies.reduce((todasEspecies, especie) => {
+    let objEspecies = todasEspecies;
     if (!species) {
       const nome = especie.name;
       const qtd = especie.residents.length;
-      // meu obj todasEspecies, na chave 'nome' recebe como valor a 'qtd'
-      todasEspecies[nome] = qtd;
-    } else {      
-        if (species === especie.name) todasEspecies = especie.residents.length;
+      // meu obj objEspecies, na chave 'nome' recebe como valor a 'qtd'      
+      objEspecies[nome] = qtd;
+    } else {
+      if (species === especie.name) objEspecies = especie.residents.length;
     }
-    return todasEspecies;
+    return objEspecies;
   }, {});
   return contagemAnimal;
 }
