0fdffcf27ed17eaac4daf79162a8a56dcafc9be1
SaraRabelo <rabelorsara@gmail.com>
2021-07-07 10:49:23 -0300

    Solução da Questão 10a, Corrigindo o lint

--- src/zoo.js
+++ src/zoo.js
@@ -77,15 +77,15 @@ function getSchedule(dayName) {
 }
 
 function getOldestFromFirstSpecies(id) {
-  const specieId = data.employees.find((employee) =>  employee.id === id).responsibleFor[0];
-  const relSpecie = data.species.find((specie) => specieId === specie.id).residents
-  .reduce((velho, resident) => {
-    if (velho.age < resident.age) {
-         return resident;
-    }
-    return velho;
-    });
-return Object.values(relSpecie);
+    const specieId = data.employees.find((employee) =>  employee.id === id).responsibleFor[0];
+    const relSpecie = data.species.find((specie) => specieId === specie.id).residents
+    .reduce((velho, resident) => {
+      if (velho.age < resident.age) {
+           return resident;
+      }
+      return velho;
+      });
+  return Object.values(relSpecie);
 }
 
 function increasePrices(percentage) {
