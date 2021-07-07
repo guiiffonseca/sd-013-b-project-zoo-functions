cc0d22cddd9f5d06281efe00770fc3f895303737
Haron Prestes <haronprestes1303@gmail.com>
2021-07-06 22:20:30 -0300

    Correçoes de lint.

--- src/zoo.js
+++ src/zoo.js
@@ -33,12 +33,12 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 function countAnimals(especies) {
   const animais = {};
-  if (!especies) {
-    return animais;
-  }
   species.forEach((especie) => {
     animais[especie.name] = especie.residents.length;
   });
+  if (!especies) {
+    return animais;
+  }
   return animais[especies];
 }
 
