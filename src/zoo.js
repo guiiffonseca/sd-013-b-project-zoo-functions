aa83598edc8ff0acf8f01527a45e050bed3f5307
Thales Vinicius <thalessilva.tv@gmail.com>
2021-07-08 15:38:26 -0300

    refatorando requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -27,7 +27,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return employees.some(({ managers }) => managers.some((manager) => manager === id));
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
