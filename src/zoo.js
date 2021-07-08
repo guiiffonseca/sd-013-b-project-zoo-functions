0145dc0745e7f4fb8b73d6717d9505371ef08bb0
gsdu-arte <gabi15duarte@hotmail.com>
2021-07-08 19:20:26 -0300

    Resoluçao do requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -1,17 +1,22 @@
 const data = require('./data');
 const { species } = require('./data');
 
-// Utilizei a feature rest para que fosse possível passar vários parâmetros ao mesmo tempo.
+// Usei rest para que fosse possível passar vários parâmetros ao mesmo tempo.
 // Usei o .filter pois retorna um novo array com os objetos que passam na regra de negócio
 // O rest cria um novo array com os valores passados para ele(por isso usei o .includes)
-// Desestruturei o id pois é uma chave de um objeto do array species
+// Desestruturei o id pois é uma chave dos objetos do array species
 function getSpeciesByIds(...ids) {
   const filterSpecies = species.filter(({ id }) => ids.includes(id));
   return filterSpecies;
 }
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+// Usei o .find para encontrar o nome da espécie passada no parâmetro animalName
+// Depois que o nome for encontrado acesso a chave .residents do mesmo objeto ao qual o nome pertence
+// Usei .every pois queria saber se todos os animais dessa espécie tem idade maios ou igual ao parâmetro animalAge
+// .every retorna um boolean
+function getAnimalsOlderThan(animalName, animalAge) {
+  return species.find(({ name }) => name === animalName)
+    .residents.every(({ age }) => age >= animalAge);
 }
 
 function getEmployeeByName(employeeName) {
