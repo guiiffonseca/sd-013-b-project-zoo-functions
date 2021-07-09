ddfad8d5857b03a29b859a63ea711d81a9ec20c6
Camila Dornas <camiladornas@gmail.com>
2021-07-08 23:01:16 -0300

    Add requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -1,13 +1,16 @@
 const { species, employees } = require('./data');
 
 const data = require('./data');
-
+// ----------- REQUISITO 01 ----------- //
 const getSpeciesByIds = (...rest) => species.filter((especie) => rest.includes(especie.id));
-
+// 1.Utilizado como parâmetro o (...rest), para possiilitar a entrega de mais de um parâmetro.
+// 2. Filtrado todas as espécies que fazem do parâmetro passado.
+// 3. Verificado se as espécies contém o id passado.
+// ----------- REQUISITO 02 ----------- //
 const getAnimalsOlderThan = (animal, age) =>
   species.find((especie) => especie.name === animal)
     .residents.every((elemento) => elemento.age >= age);
-
+// ----------- REQUISITO 03 ----------- //
 const getEmployeeByName = (employeeName) => {
   if (!employeeName) {
     return {};
@@ -15,15 +18,11 @@ const getEmployeeByName = (employeeName) => {
   return employees.find((nome) =>
     (nome.firstName === employeeName || nome.lastName === employeeName));
 };
-
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
-
-function isManager(id) {
-  // seu código aqui
-}
-
+// ----------- REQUISITO 04 ----------- //
+const createEmployee = (personalInfo, associatedWith) => ({ ...personalInfo, ...associatedWith });
+// ----------- REQUISITO 05 ----------- //
+const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
+// ----------- REQUISITO 06 ----------- //
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
