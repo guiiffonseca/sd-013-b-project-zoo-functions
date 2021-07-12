d44ea642b5486ac32eb9805c38a43b60d723fa0c
Vitor <imbroisi.vitor@gmail.com>
2021-07-12 12:47:05 -0300

    lint

--- src/zoo.js
+++ src/zoo.js
@@ -114,8 +114,8 @@ function getOldestFromFirstSpecies(id) {
 function increasePrices(percentage) { // fizemos um for in para percorrer os valores dentro do prices, depois bastou fazer a regra matemática
   // for (const key in prices) { // primeiro eu fiz com for in, no npm test passou, mas o lint não deixou, não entendi exatamente porque. VOLTAR TIRAR ESA DÚVIDA.
   const price = Object.keys(prices);
-  price.forEach((priveValue) =>
-  prices[priveValue] = Math.round((prices[priveValue] * 100) * ((percentage / 100) + 1)) / 100); // VOTLAR NESTA LÓGICA MATEMÁTICA, PESSOAL ME AJUDOU NO GRUPO DO ZAP, ENTENDI A LÓGICA TODA, PORÉM NÃO ESTÁ CLARO O PORQUE DO JS NÃO ACEITAR ISSO POR EXEMPLO: prices[key] = (prices[key] + (prices[key] * (20 / 100)))) OU PORQUE DIVIDIR E MULTIPLICAR POR 100 ANTES E DEPOIS. VOLTAR PARA REVER O CONTEÚDO E SE APROFUNDAR MAIS.
+  price.forEach((priveValue) => {
+    prices[priveValue] = Math.round((prices[priveValue] * 100) * ((percentage / 100) + 1)) / 100}); // VOTLAR NESTA LÓGICA MATEMÁTICA, PESSOAL ME AJUDOU NO GRUPO DO ZAP, ENTENDI A LÓGICA TODA, PORÉM NÃO ESTÁ CLARO O PORQUE DO JS NÃO ACEITAR ISSO POR EXEMPLO: prices[key] = (prices[key] + (prices[key] * (20 / 100)))) OU PORQUE DIVIDIR E MULTIPLICAR POR 100 ANTES E DEPOIS. VOLTAR PARA REVER O CONTEÚDO E SE APROFUNDAR MAIS. 
 }
 
 // increasePrices()
