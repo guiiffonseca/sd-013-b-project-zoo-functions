3b2e5613da2851cba478430fe2c94edd6ab1b39a
Diego Costa <dgrcosta@gmail.com>
2021-07-12 12:30:23 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -29,8 +29,15 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  employees.push(newEmployee);
 }
 
 function countAnimals(species) {
