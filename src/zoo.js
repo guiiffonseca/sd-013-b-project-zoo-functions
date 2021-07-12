73ea881d44ae01c896da1d81a8aa012d60fae692
Guilherme <guilhermerg1234@gmail.com>
2021-07-11 21:42:52 -0300

    LINT

--- src/zoo.js
+++ src/zoo.js
@@ -2,7 +2,7 @@ const { species, employees } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) { // ... = Parâmetro REST => Faz com que a função receba n parâmetros. Como pega todos os animais, fica mais de 1 parâmetro, aí puxa tudo com o rest.
-  const animalList = species.filter(( species, index) => species.id === ids[index]);
+  const animalList = species.filter((index) => species.id === ids[index]);
   return animalList;
 }
 console.log(species);
