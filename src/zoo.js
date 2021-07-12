29d1523bd66e8ffa7d484316022dce47cc644a07
Vitor <imbroisi.vitor@gmail.com>
2021-07-12 01:20:45 -0300

    lint

--- src/zoo.js
+++ src/zoo.js
@@ -85,15 +85,14 @@ function getSchedule(dayName) { // Pessoal da sala da estudo me ajudou a fazer (
   selectDay.forEach((hourValue) => { // pegamos o array com os dias da semana e iteramos ele com forEach, tomando cuidado pois a segunda feira é um caso diferente pois está closed.
     if (hourValue === 'Monday') {
       weekDay[hourValue] = 'CLOSED';
-      } else { 
-      weekDay[hourValue] = 
-      (`Open from ${hours[hourValue].open}am until ${hours[hourValue].close - 12}pm`); // montamos a template literal com as possíveis saidas iterando com o forEach.
-      }
+    } else {
+      weekDay[hourValue] = (`Open from ${hours[hourValue].open}am until ${hours[hourValue].close - 12}pm`); // montamos a template literal com as possíveis saidas iterando com o forEach.
+    }
   });
   if (arguments.length === 0) { // montamos a lógica para caso não seja retornado nenhum parametro, será retornado o objeto weekday que já foi preenchido com os dias na parte de cima do código.
-  return weekDay;
+    return weekDay;
   } else {
-  return { [dayName]: weekDay[dayName] }; // lógica para se apenas um dia for passado.
+    return { [dayName]: weekDay[dayName] }; // lógica para se apenas um dia for passado.
   }
 }
 console.log(getSchedule());
