cd830c740a01b175ac1b4919c329c254c33cf359
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-07-13 19:15:07 -0300

    addEmployee completo e descricoes do codigo

--- src/zoo.js
+++ src/zoo.js
@@ -3,37 +3,47 @@ const data = require('./data');
 
 // eslint-disable-next-line no-shadow
 function getSpeciesByIds(...ids) {
-  // seu código aqui
+  // Retorna o id das especies
   const idReturn = species.filter((specie) => ids.includes(specie.id));
   return idReturn;
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // Retorna a idade do animal
   const animalAge = species.find((specie) => specie.name === animal).residents
     .every((index) => index.age >= age);
   return animalAge;
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // Retorna o empregado 
   if (employeeName === undefined) return {};
   return employees.find((thing) =>
     thing.firstName === employeeName || thing.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  // Cria um employee
   const emptyArray = {};
   return Object.assign(emptyArray, personalInfo, associatedWith);
 }
 
 function isManager(id) {
-  // seu código aqui
+  // Verifica se o employee e gerente
+  return employees.some((employee) => employee.manager === id);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  // Adiciona um novo empregado no fim do array
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  data.employees.push(newEmployee);
+  return data.employees;
 }
 
 function countAnimals() {
