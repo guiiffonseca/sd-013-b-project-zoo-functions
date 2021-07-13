d1e1a56167cbb93389a47d24fff79f0971860221
VictorFrancozi <victor.francozi@gmail.com>
2021-07-13 14:43:11 -0300

    Function addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -23,8 +23,14 @@ function isManager(id) {
   return employees.some(({ managers }) => managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function countAnimals(species) {
