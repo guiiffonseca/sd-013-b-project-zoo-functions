4f140cc5ab6b096d28ead00076edac025d555852
SaraRabelo <rabelorsara@gmail.com>
2021-07-14 18:30:29 -0300

    Questão 10a corrigindo Lint

--- src/zoo.js
+++ src/zoo.js
@@ -74,22 +74,21 @@ function getAnimalMap(options) {
 
 function getSchedule(dayName) {
   const retorno = {};
-    if (!dayName) {
-      Object.entries(data.hours).forEach((day) => {
-        retorno[day[0]] = `Open from ${day[1].open}am until ${day[1].close - 12}pm`;
-        if (day[0] === 'Monday') {
-          retorno[day[0]] = 'CLOSED';
-        }
-      });
-      return retorno;
-    }
-    const hourDay = Object.entries(data.hours).find((day) => day[0] === dayName);
-    retorno[hourDay[0]] = `Open from ${hourDay[1].open}am until ${hourDay[1].close - 12}pm`;
-    if (hourDay[0] === 'Monday') {
-      retorno[hourDay[0]] = 'CLOSED';
-    }
+  if (!dayName) {
+    Object.entries(data.hours).forEach((day) => {
+      retorno[day[0]] = `Open from ${day[1].open}am until ${day[1].close - 12}pm`;
+      if (day[0] === 'Monday') {
+        retorno[day[0]] = 'CLOSED';
+      }
+    });
     return retorno;
-  
+  }
+  const hourDay = Object.entries(data.hours).find((day) => day[0] === dayName);
+  retorno[hourDay[0]] = `Open from ${hourDay[1].open}am until ${hourDay[1].close - 12}pm`;
+  if (hourDay[0] === 'Monday') {
+    retorno[hourDay[0]] = 'CLOSED';
+  }
+  return retorno;
 }
 
 function getOldestFromFirstSpecies(id) {
