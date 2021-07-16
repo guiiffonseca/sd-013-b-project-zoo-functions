02962563c39b22ec8d70e312c25cee94a875824d
Isaddora Bayma <isadora.bayma@gmail.com>
2021-07-15 22:56:37 -0300

    requisito 7 in progress

--- src/zoo.js
+++ src/zoo.js
@@ -5,7 +5,7 @@ function getSpeciesByIds(...speciesIds) {
   return speciesIds.map((specieId) => species.find((specie) => specie.id === specieId));
 }
 
-//  console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   const specie = species.find((grup) => grup.name === animal).residents;
@@ -45,9 +45,77 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   return employees.push(newEmployee);
 }
 
-function countAnimals(species2) {
-  // seu código aqui
+function creatingArrayAnimalNames() {
+  const array = [];
+  species.map((specie) => array.push(specie.name));
+  return array;
 }
+
+console.log(creatingArrayAnimalNames());
+
+// ^^ funcao de dependecia do countAnimals ^^
+function createArrayAnimalCount(array) {
+  species.map((specie) => array.push(specie.residents.length));
+}
+// ^^ funcao de dependecia do countAnimals ^^
+
+function countAnimals(species2) {
+  // quando vazio retornar um objeto desse tipo: key = name ; value = residents.length for each
+  if (species2 === undefined) {
+    const arr = [];
+    // creatingArrayAnimalNames(arr);
+
+    // const allAnimaisCount = ;
+  } else {
+    // resgatar o objeto da specie desejada pelo nome
+    const eachSpecie = species.find((specie) => specie.name === species2).residents;
+    // retornar o residents.length
+    return eachSpecie.length;
+  }
+}
+
+countAnimals();
+// {
+//   'lions': 4,
+//   'tigers': 2,
+//   'bears': 3,
+//   'penguins': 4,
+//   'otters': 4,
+//   'frogs': 2,
+//   'snakes': 2,
+//   'elephants': 4,
+//   'giraffes': 6
+// };
+
+// const student1 = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkills: 'Ótimo',
+// };
+
+// const student2 = {
+//   Html: 'Bom',
+//   Css: 'Ótimo',
+//   JavaScript: 'Ruim',
+//   SoftSkills: 'Ótimo',
+//   Git: 'Bom', // chave adicionada
+// };
+
+// const listSkills = (student) => {
+//   const arrayOfSkills = Object.keys(student);
+//   for(index in arrayOfSkills){
+//     console.log(`${arrayOfSkills[index]}, Nível: ${student[arrayOfSkills[index]]}`);
+//   }
+// };
+
+// console.log('Estudante 1');
+// listSkills(student1);
+
+// console.log('Estudante 2');
+// listSkills(student2);
+
+// // /////////////////////////////////////////////////////////////
 
 function calculateEntry(entrants) {
   // seu código aqui
