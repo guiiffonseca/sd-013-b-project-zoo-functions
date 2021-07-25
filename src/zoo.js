ca97ac2b9f9aaf0cd54e309c93260fcd69e2950a
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-07-25 19:11:04 -0300

    Soluciona requisito 12

--- src/zoo.js
+++ src/zoo.js
@@ -110,7 +110,9 @@ function getOldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  Object.keys(prices).forEach((price) => {
+    prices[price] = Math.round((prices[price] + (percentage / 100) * prices[price]) * 100) / 100;
+  });
 }
 
 function getEmployeeCoverage(idOrName) {
