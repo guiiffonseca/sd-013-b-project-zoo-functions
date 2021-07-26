8b38cb903213af8ac5d477b719f4af4cb8bf9e9f
GuCanalle <gucanalle@gmail.com>
2021-07-26 20:38:03 -0300

    Concluido requisito 11

--- src/zoo.js
+++ src/zoo.js
@@ -75,6 +75,21 @@ function getSchedule(dayName) {
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = data.employees.find((worker) => worker.id === id);
+  const animalId = data.species.find((specie) => specie.id === employee.responsibleFor[0]);
+  let age = 0;
+  let oldestAnimal;
+  animalId.residents.forEach((animal) => {
+    if (age < animal.age) {
+      age = animal.age;
+      oldestAnimal = [];
+      oldestAnimal.push(animal.name);
+      oldestAnimal.push(animal.sex);
+      oldestAnimal.push(animal.age);
+      return oldestAnimal;
+    }
+  });
+  return oldestAnimal;
 }
 
 function increasePrices(percentage) {
