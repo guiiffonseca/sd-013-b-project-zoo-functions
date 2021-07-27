1bc9195cbfa370500794a13364dc2b2d4e8e78c4
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-07-27 11:05:51 -0300

    Inclusao de referencias

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 function countAll() {
   const object = {};
   species.forEach((specie) => {
-    Object.assign(object, {
-      [specie.name]: specie.residents.length,
-    });
+    object[specie.name] = specie.residents.length;
   });
   return object;
 }
@@ -52,6 +50,9 @@ function countAnimals(specieName) {
   return (!specieName) ? countAll()
     : species.find((specie) => specie.name === specieName).residents.length;
 }
+
+// A função a seguir foi feita com base na resposta do usuário faboulaws na seguinte thread
+// (https://stackoverflow.com/questions/15748656/javascript-reduce-on-object)
 
 function calculateEntry(entrants) {
   return (!entrants) ? 0
@@ -107,6 +108,9 @@ function getOldestFromFirstSpecies(id) {
     .sort((animal1, animal2) => animal2.age - animal1.age)[0];
   return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
+
+// Para acertar os decimais como pedido no teste, foi usado essa thread como referênciana função a seguir
+// (https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary)
 
 function increasePrices(percentage) {
   Object.keys(prices).forEach((price) => {
